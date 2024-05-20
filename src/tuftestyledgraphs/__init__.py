3cafbfa9d8348d84662bc3d3a5d82e7a398de1f8
Warner Leijenaar <git@renraw.nl>
2024-05-20 20:37:27 +0200

    Added  StepGraph to the default imports

--- src/tuftestyledgraphs/__init__.py
+++ src/tuftestyledgraphs/__init__.py
@@ -1,8 +1,13 @@
 from importlib.metadata import version
 
+from .enums import StepEnum  # noqa
 from .graphs.bar import HorizontalBarGraph, VerticalBarGraph  # noqa
 from .graphs.line import LineGraph  # noqa
 from .graphs.scatter import ScatterGraph  # noqa
+from .graphs.step import (  # noqa
+    StepEnum,  # noqa
+    StepGraph,  # noqa
+)
 from .stylesheets import TufteStyleSheet  # noqa
 
 __version__: str = version(__package__)
